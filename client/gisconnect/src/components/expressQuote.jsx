bf62a7abfc05512f0b125f3ff593ad7305da31b2
MaryJo Albanes <maryjo@MaryJos-MacBook-Pro.local>
2025-09-01 12:54:38 -0600

    expressQuote.jsx pdf desgin modifs

--- client/gisconnect/src/components/expressQuote.jsx
+++ client/gisconnect/src/components/expressQuote.jsx
@@ -777,7 +777,7 @@ export default function ExpressQuote() {
     // Totals text
     let line = 0;
     const lineH = 5;
-    const rightX = 190; // ~ inside box
+    const rightX = 10; // ~ inside box
     // const rightX = 196; // ~ inside box
   
     doc.setFontSize(10);
