264932c8561fad95583cd5cf2ff6a8d4337f223d
MaryJo Albanes <maryjo@MaryJos-MacBook-Pro.local>
2025-09-18 11:51:05 -0600

    modifs to roles

--- server/notifications/roles.js
+++ server/notifications/roles.js
@@ -1,29 +1,26 @@
 // server/notifications/roles.js
 const FULL_ACCESS = new Set([
     "majo_test@gmail.com",
-    // "ventas@greenimportsol.com",
-    // "info@greenimportsol.com",
+    "ventas@greenimportsol.com",
+    "info@greenimportsol.com",
   ]);
   
   const ADMIN_FACTURAS_Y_LOGISTICA = new Set([
     "majo_test@gmail.com",
-
-    // "administracion@greenimportsol.com",
-    // "administracion2@greenimportsol.com",
+    "administracion@greenimportsol.com",
+    "administracion2@greenimportsol.com",
   ]);
   
   const LOGISTICA_Y_ALMACEN = new Set([
     "majo_test@gmail.com",
-
-    // "logistica@greenimportsol.com",
-    // "almacen@greenimportsol.com",
-    // "almacen2@greenimportsol.com",
+    "logistica@greenimportsol.com",
+    "almacen@greenimportsol.com",
+    "almacen2@greenimportsol.com",
   ]);
   
   const ALMACEN_LIMITADO = new Set([
     "majo_test@gmail.com",
-
-    // "almacen3@greenimportsol.com",
+    "almacen3@greenimportsol.com",
   ]);
   
   const STAGES = {
