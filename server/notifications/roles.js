3e3f55f99118d9f7d50e83797b2ad9bb63894253
MaryJo Albanes <maryjo@MaryJos-MacBook-Pro.local>
2025-09-18 10:44:12 -0600

    modifs to roles.js

--- server/notifications/roles.js
+++ server/notifications/roles.js
@@ -1,30 +1,27 @@
 // server/notifications/roles.js
 const FULL_ACCESS = new Set([
     "majo_test@gmail.com",
-    // "ventas@greenimportsol.com",
-    // "info@greenimportsol.com",
+    "ventas@greenimportsol.com",
+    "info@greenimportsol.com",
   ]);
   
   const ADMIN_FACTURAS_Y_LOGISTICA = new Set([
-    "majo_test@gmail.com",
-    // "administracion@greenimportsol.com",
-    // "administracion2@greenimportsol.com",
+    "administracion@greenimportsol.com",
+    "administracion2@greenimportsol.com",
   ]);
   
   const LOGISTICA_Y_ALMACEN = new Set([
-    "majo_test@gmail.com",
-    // "logistica@greenimportsol.com",
-    // "almacen@greenimportsol.com",
-    // "almacen2@greenimportsol.com",
+    "logistica@greenimportsol.com",
+    "almacen@greenimportsol.com",
+    "almacen2@greenimportsol.com",
   ]);
   
   const ALMACEN_LIMITADO = new Set([
-    "majo_test@gmail.com",
-    // "almacen3@greenimportsol.com",
+    "almacen3@greenimportsol.com",
   ]);
   
   const STAGES = {
-    PEDIDO_REALIZADO: "PEDIDO_REALIZADO",       // <-- NEW
+    PEDIDO_REALIZADO: "PEDIDO_REALIZADO",
     EVIDENCIA_DE_PAGO: "EVIDENCIA_DE_PAGO",
     PAGO_VERIFICADO: "PAGO_VERIFICADO",
     PREPARANDO_PEDIDO: "PREPARANDO_PEDIDO",
@@ -35,28 +32,23 @@ const FULL_ACCESS = new Set([
   function recipientsForStage(stage) {
     const emails = new Set();
     switch (stage) {
-      case STAGES.PEDIDO_REALIZADO:            // <-- NEW
+      case STAGES.PEDIDO_REALIZADO:
         FULL_ACCESS.forEach(e => emails.add(e));
         break;
-  
       case STAGES.EVIDENCIA_DE_PAGO:
         FULL_ACCESS.forEach(e => emails.add(e));
         break;
-  
       case STAGES.PAGO_VERIFICADO:
         ADMIN_FACTURAS_Y_LOGISTICA.forEach(e => emails.add(e));
         LOGISTICA_Y_ALMACEN.forEach(e => emails.add(e));
         ALMACEN_LIMITADO.forEach(e => emails.add(e));
         break;
-  
       case STAGES.PREPARANDO_PEDIDO:
         ADMIN_FACTURAS_Y_LOGISTICA.forEach(e => emails.add(e));
         break;
-  
       case STAGES.ETIQUETA_GENERADA:
         LOGISTICA_Y_ALMACEN.forEach(e => emails.add(e));
         break;
-  
       case STAGES.PEDIDO_ENTREGADO:
         FULL_ACCESS.forEach(e => emails.add(e));
         break;
