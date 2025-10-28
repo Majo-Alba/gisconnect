6364d60b54d7aebd82b7de2e18ae7aa49b24b37c
MaryJo Albanes <maryjo@MaryJos-MacBook-Pro.local>
2025-10-28 16:40:35 -0600

    packDetails modifs

--- client/gisconnect/src/components/packDetails.jsx
+++ client/gisconnect/src/components/packDetails.jsx
@@ -1,5 +1,5 @@
 // Hey chatgpt, in my packDetails.jsx file, the person packing the order is prompted to upload an image. When uploading using an iphone, everything works smoothly, but when trying to upload through android, it allows me to take a picture and correctly handles it but doesnt allow me to open photo gallery to select a photo from there. Keep in mind that person packing can upload up to 3 pictures from gallery 
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 
